c1445b7c44b231cc9730f8ad58ad2b0013f351f4
Keitaro Komatsu <kin29sasuke@outlook.com>
2025-10-19 19:15:42 +0900

    add README

--- src/embedder.py
+++ src/embedder.py
@@ -3,7 +3,7 @@ from sentence_transformers import SentenceTransformer
 
 
 def create_embeddings(docs):
-    model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
+    model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2", device="cpu")
     embeddings = model.encode(docs, convert_to_numpy=True)
     index = faiss.IndexFlatL2(embeddings.shape[1])
     index.add(embeddings)
